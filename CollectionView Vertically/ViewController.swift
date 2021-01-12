7ef4bcab12d8122363dfd738852a8694b11b08aa
Soft <rafeeqrahman1997@gmail.com>
2021-01-12 05:31:18 +0530

    Custom Vertical Collection View

---
+++ CollectionView Vertically/ViewController.swift
@@ -0,0 +1,91 @@
+
+import UIKit
+
+struct CustomData {
+    var title : String
+    var image : UIImage
+    var url : String
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var blue: UIView!
+    let data = [
+        CustomData(title: "one", image: #imageLiteral(resourceName: "5"), url: "www.google.com"),
+        CustomData(title: "one", image: #imageLiteral(resourceName: "3"), url: "www.google.com"),
+        CustomData(title: "two", image: #imageLiteral(resourceName: "3"), url: "www.google.com"),
+        CustomData(title: "three", image: #imageLiteral(resourceName: "4"), url: "www.google.com")
+    ]
+
+    fileprivate let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame:  .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+        return cv
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .white
+        collectionView.topAnchor.constraint(equalTo: blue.bottomAnchor, constant: 40).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5).isActive = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2.5, height: collectionView.frame.width/2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+        cell.data = self.data[indexPath.row]
+        return cell
+    }
+}
+
+class CustomCell: UICollectionViewCell {
+    
+    var data: CustomData? {
+        didSet {
+            guard let data = data else { return }
+            bg.image = data.image
+        }
+    }
+    
+    fileprivate let bg: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "1")
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 12
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        contentView.addSubview(bg)
+        bg.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        bg.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        bg.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        bg.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
